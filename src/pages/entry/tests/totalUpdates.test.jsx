c3a528f33b6da27bae8a0a6b75e19a7644f1fb13
Ivan Karnaukhov <i007300@yandex.ru>
2021-09-02 15:32:22 -0700

    stuck at lecture 62 with  Error: Uncaught [TypeError: undefined is not a function]

---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+
+test('updates scoop subtotal when scoops change', async () => {
+  render(<Options optionsType="scoops" />);
+
+  //make sure total starts out $0.00
+  const scoopsSubtotal = screen.getByText('Scoops total $', { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent('0.00');
+
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1'); //the number should be a string
+  expect(vanillaInput).toHaveTextContent('2.00'); //subtotal started out as $2
+});
+
+test('update chocolate scoops to two', async () => {
+  render(<Options optionsType="scoops" />);
+
+  // update chocolate scoops to 2 and check the subtotal
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, '2');
+  expect(chocolateInput).toHaveTextContent('4.00'); //sup totals is $2 + $4 we should now have subtotal of $6
+});
