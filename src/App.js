d493eb73124997ed73256f5771a06c8cfe24f3b1
Ivan Karnaukhov <i007300@yandex.ru>
2021-09-04 14:47:52 -0700

    everything is up to date from  /sundaes-on-demand/03-toppings-subtotal/

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Container from 'react-bootstrap/Container';
-import { OrderDetailsProvider } from './context/OrderDetails';
 import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider } from './contexts/OrderDetails';
 
 function App() {
-	return (
-		<Container>
-			<OrderDetailsProvider>
-				{/* Summary page and entry page need provider */}
-				<OrderEntry />
-			</OrderDetailsProvider>
-			{/* confirmation page does not need provider */}
-		</Container>
-	);
+  return (
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
+  );
 }
 
 export default App;
